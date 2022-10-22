a85982df6073cc3ee5994c53fb7f25ad6baa99ad
Patryk Janicki <patrykjanicki@icloud.com>
2022-10-22 08:59:12 +0200

    fixing problem with mongodb

--- index.js
+++ index.js
@@ -4,25 +4,29 @@ const port = 3000
 const path = require("path")
 const ejsLayouts = require("express-ejs-layouts")
 const app = express()
-
 // view engine
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname + "/views"))
 // set layout
 app.use(ejsLayouts)
 app.set("layout", "layouts/main")
+//public folder
+app.use(express.static("public"))
 
 app.get("/", (req, res) => {
+	console.log(req.url)
 	res.render("pages/home", {
 		title: "Strona główna",
+		url: req.url,
 	})
 })
 
 app.get("/firmy/:name", (req, res) => {
+	console.log(req.url)
 	const { name } = req.params
 	const companies = [
-		{ slug: "tworcastron", name: "Tworca Stron.pl" },
-		{ slug: "brukmode", name: "Bruk Mode" },
+		{ slug: "abcd", name: "A B C D" },
+		{ slug: "efgh", name: "E F G H" },
 	]
 
 	const company = companies.find(x => x.slug === name)
